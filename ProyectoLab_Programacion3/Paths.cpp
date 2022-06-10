29f645e91cebc5bfc57acef705634cc808a077b7
FRANKLIN XAVIER RODRIGUEZ TINOCO <xaviertinoco006@unitec.edu>
2022-06-09 21:40:28 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoLab_Programacion3/Paths.cpp
@@ -0,0 +1,29 @@
+#include "Paths.h"
+
+Paths::Paths():Npc(NULL,0,0,0,0,nullptr)
+{
+
+
+
+
+}
+
+Paths::Paths(ALLEGRO_BITMAP* _Sprite, int x, int y, int h, int w, PokemonNames name,Player*Player) :Npc(_Sprite,x, y,h,w, Player)
+{
+
+	Name = name;
+
+	//Tener cuidado con player
+
+
+
+
+
+
+
+
+
+
+
+	
+}
